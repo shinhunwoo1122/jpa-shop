f8abc483c1a14797b9dcec82df4b02da32592dce
shinhunwoo1122 <shinhunwoo1122@gmail.com>
2023-08-03 16:04:30 +0900

    -project 생성 -프로젝트 클래스 도메인 설계 -회원 등록 -회원 조회 리스트 -상품 등록 -상품 수정 -주문 등록 -주문 취소 -주문 리스트 조회

---
+++ src/test/java/jpabook/jpashop/service/ItemUpdateTest.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop.service;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+
+@SpringBootTest
+public class ItemUpdateTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    void updateTest() throws Exception{
+
+
+    }
+}
