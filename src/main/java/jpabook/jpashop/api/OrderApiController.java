64526de2372343828afb464c4cb50f5d686ae959
shinhunwoo1122 <shinhunwoo1122@gmail.com>
2023-08-08 15:48:09 +0900

    ------ JPA 활용 ----------------
    -project 생성
    -프로젝트 클래스 도메인 설계
    -회원 등록
    -회원 조회 리스트
    -상품 등록
    -상품 수정
    -주문 등록
    -주문 취소
    -주문 리스트 조회
    
    ------ JPA API 개발 활용 -------------------
    
    -회원 등록 API
    -회원 수정 API
    -회원 조회 API
    -샘플 데이터 등록
    
    @ManyToOne @OneToOne 조회 방식
    -엔티티를 직접 노출
    (@JsonIgnore 양방향중 한방향에는 걸어야함
     Hibernate5Module Bean 등록 하여 사용함
    실무 사용x 엔티티 변화시 API 스팩 변경으로 인해 DTO 사용 할 것)
    -엔티티 조회 내용을 Dto 변환 (실무 사용시 Dto 또한 묶어서 사용하는 것이 좋음 배열형태로 바로 나가는 것 x)
    -패치 조인
    (위 내용시 PatchType.EAGER 즉시 로딩시 예상치 못하게 쿼리가 여러번 호출 되는 경우가 있고 또한 필요치 않은곳에서 JOIN이 일어나 성능 저하 PatchType.LAZE 지연 로딩시 처음에는 쿼리가 한방 나가지만 추가적인 연관관계 출력시
     프록시객체에 채워넣는 형태로 쿼리가 날라감 N+1 성능 저하가 이루어 질 수 있음 그렇기 떄문에 해당 내용 조회시
     fetch join 으로 한번에 join 하여 쿼리 생성 후 1차 캐시에 영속하여 저장하여 사용 성능 향상 됨)
     -dto로 조회
     (fit하게 설정되어있고 이미 repository에 api 스팩이 들어가기 떄문에 분리하여 관리하는게 좋음)
    
    -컬렉션조회 최적화
    -전체 조회 (N+1) 노출 됨
    -distinct 사용 DB와 다르게 모든 중복 데이터가 있을시 중복 제거가 아닌 jpa 에서 메모리에서 key 값 으로 중복 값 제거
     (페이징 처리는 불가 함)
    -To One 부분만 fetch join 사용 후 나머지 지연로딩 으로 조히 (쿼리 여러번 실행 성능 저하)
    -default_batch_fetch_size 사용 지연로딩 된 부분의 쿼리를 in 절로 한번에 묶어서 가져옴 1+N을 1+1로 변환 가능
     (물론 데이터가 너무 많을시 fetch_size에 따라 +실행 될 수 있음 100~1000 1000이상 사용시 DataBase 마다 에러로 인식 할 수 있다고 함 Was에서는 100이든 1000이든 결국 전체 조회를 하기때문에 100과 1000의 성능은 차이가 없고
      순간적인 과부화로인한 DB쪽 메모리와 cpu환경을 체크하여 적절하게 조절하여 사용 해야 함)
    
    -JPA에서 DTO 바로 조회 new 생성자를 통해 해당 dto 경로와 값들을 직접 채워 놓고
     하나의 플랫한 한줄데이터로 조회해온 후 반복문을 통해 컬렉션 조회 쿼리를 날려 채워 넣어 리턴함
     N+1상태가 되어 버리긴 함
    
    -쿼리 조건을 where = 조건이 아닌 List로 전체 아이디 값을 뽑은 후에 in 절로 한번에 조회
     1+1상태로 만들어 줄 수 있음 전체 조회 후 키 값으로 조회
     해당 조회 값을 map stream 으로 돌려 만든 후에 해당 전체 조회 값에 메모리에서 반복문으로 아이디 값을 통해 주입
    
    -전체를 fetch join으로 증가된 데이터를 직접 발라내어 Dto으로 변환함
     장점은 쿼리가 한번 출력되지만 1대 다로 조인이 되버려 값이 늘어난 상태의 데이터를 다시 맞춰야 하기때문에
     로직이 늘어나고 데이터가 많을경우에 성능 저하의 단점을 가짐

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -7,7 +7,10 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
-import lombok.Data;
+import jpabook.jpashop.repository.order.query.OrderFlatDto;
+import jpabook.jpashop.repository.order.query.OrderItemQueryDto;
+import jpabook.jpashop.repository.order.query.OrderQueryDto;
+import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,13 +19,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
 
 @RestController
 @RequiredArgsConstructor
 public class OrderApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
 
     @GetMapping("/api/v1/orders")
     public List<Order> ordersV1(){
@@ -41,7 +46,7 @@ public class OrderApiController {
         List<Order> orders = orderRepository.findAllByString(new OrderSearch());
         List<OrderDto> result = orders.stream()
                 .map(o -> new OrderDto(o))
-                .collect(Collectors.toList());
+                .collect(toList());
         return result;
     }
 
@@ -50,7 +55,7 @@ public class OrderApiController {
         List<Order> orders = orderRepository.findAllWithItem();
         List<OrderDto> result = orders.stream()
                 .map(o -> new OrderDto(o))
-                .collect(Collectors.toList());
+                .collect(toList());
         return result;
     }
 
@@ -61,9 +66,34 @@ public class OrderApiController {
 
         List<OrderDto> result = orders.stream()
                 .map(o -> new OrderDto(o))
-                .collect(Collectors.toList());
+                .collect(toList());
 
         return result;
+    }
+
+    @GetMapping("/api/v4/orders")
+    public List<OrderQueryDto> ordersV4(){
+        return orderQueryRepository.findOrderQueryDtos();
+    }
+
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5(){
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
+    @GetMapping("/api/v6/orders")
+    public List<OrderQueryDto> ordersV6(){
+        List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
+        return flats.stream()
+                .collect(groupingBy(o -> new OrderQueryDto(o.getOrderId(),
+                                o.getName(), o.getOrderDate(), o.getOrderStatus(), o.getAddress()),
+                        mapping(o -> new OrderItemQueryDto(o.getOrderId(),
+                                o.getItemName(), o.getOrderPrice(), o.getCount()), toList())
+                )).entrySet().stream()
+                .map(e -> new OrderQueryDto(e.getKey().getOrderId(),
+                        e.getKey().getName(), e.getKey().getOrderDate(), e.getKey().getOrderStatus(),
+                        e.getKey().getAddress(), e.getValue()))
+                .collect(toList());
     }
 
     @Getter
@@ -86,7 +116,7 @@ public class OrderApiController {
             /*order.getOrderItems().stream().forEach(o -> o.getItem().getName());
             orderItems = order.getOrderItems();*/
             orderItems = order.getOrderItems().stream().map(orderItem -> new OrderItemDto(orderItem))
-                    .collect(Collectors.toList());
+                    .collect(toList());
         }
     }
 
